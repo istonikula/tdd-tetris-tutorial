9b5a88260ad35d1b9ee44e53ea20f35057f4b89a
Isto Nikula <isto.nikula@iki.fi>
2015-04-17 12:51:50 +0300

    RotatingTetrominoesTest The_I_shape can_be_rotated_left_once

--- src/main/java/tetris/Tetromino.java
+++ src/main/java/tetris/Tetromino.java
@@ -1,34 +1,63 @@
 package tetris;
 
 public class Tetromino {
-    public static final Tetromino T_SHAPE = new Tetromino(new Piece(
-            ".T.\n" +
-            "TTT\n" +
-            "...\n"));
+    public static final Tetromino T_SHAPE = new Tetromino(
+            new Piece(
+                ".T.\n" +
+                "TTT\n" +
+                "...\n"),
+            new Piece(
+                ".T.\n" +
+                ".TT\n" +
+                ".T.\n"),
+            new Piece(
+                "...\n" +
+                "TTT\n" +
+                ".T.\n"),
+            new Piece(
+                ".T.\n" +
+                "TT.\n" +
+                ".T.\n")
+    );
 
-    public static final Tetromino I_SHAPE = new Tetromino(new Piece(
-            ".....\n" +
-            ".....\n" +
-            "IIII.\n" +
-            ".....\n" +
-            ".....\n"));
+    public static final Tetromino I_SHAPE = new Tetromino(
+            new Piece(
+                ".....\n" +
+                ".....\n" +
+                "IIII.\n" +
+                ".....\n" +
+                ".....\n"),
+            new Piece(
+                "..I..\n" +
+                "..I..\n" +
+                "..I..\n" +
+                "..I..\n" +
+                ".....\n")
+    );
 
-    private final Piece piece;
+    private final Piece[] pieces;
+    private final int position;
 
-    private Tetromino(Piece p) {
-        piece  = p;
+    private Tetromino(Piece... p) {
+        this(0, p);
+
+    }
+
+    public Tetromino(int pos, Piece[] pieces) {
+        position = pos;
+        this.pieces  = pieces;
     }
 
     public Tetromino rotateRight() {
-        return new Tetromino(piece.rotateRight());
+        return new Tetromino(((position+1) % pieces.length), pieces);
     }
 
     public Tetromino rotateLeft() {
-        return new Tetromino(piece.rotateLeft());
+        return rotateRight().rotateRight().rotateRight();
     }
 
     @Override
     public String toString() {
-        return piece.toString();
+        return pieces[position].toString();
     }
 }
