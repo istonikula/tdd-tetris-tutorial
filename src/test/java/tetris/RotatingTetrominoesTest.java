b81b5edb26c65aea163ec50b7bc35b8b824be5b5
Isto Nikula <isto.nikula@iki.fi>
2015-04-17 12:56:37 +0300

    RotatingTetrominoesTest The_O_shape cannot_be_rotated_left

--- src/test/java/tetris/RotatingTetrominoesTest.java
+++ src/test/java/tetris/RotatingTetrominoesTest.java
@@ -175,13 +175,13 @@ public class RotatingTetrominoesTest extends Assert {
                     "...\n", shape.toString());
         }
 
-//        @Test
-//        public void cannot_be_rotated_left() {
-//            shape = shape.rotateLeft();
-//            assertEquals("" +
-//                    ".OO\n" +
-//                    ".OO\n" +
-//                    "...\n", shape.toString());
-//        }
+        @Test
+        public void cannot_be_rotated_left() {
+            shape = shape.rotateLeft();
+            assertEquals("" +
+                    ".OO\n" +
+                    ".OO\n" +
+                    "...\n", shape.toString());
+        }
     }
 }
