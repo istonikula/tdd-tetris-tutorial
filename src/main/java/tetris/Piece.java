7acaa6588e34d6c3cdb23615046af2a509aed58b
Isto Nikula <isto.nikula@iki.fi>
2015-04-17 11:02:06 +0300

    RotatingPiecesOfBlocksTest A_piece_of_3x3_blocks consists_of_many_blocks: refactor to contain blocks

--- src/main/java/tetris/Piece.java
+++ src/main/java/tetris/Piece.java
@@ -1,14 +1,41 @@
 package tetris;
 
 public class Piece {
-    private final String s;
+    private Block[][] blocks;
 
     public Piece(String s) {
-        this.s = s;
+        String[] split = s.split("\n");
+        blocks = new Block[split.length][split[0].length()];
+
+        for (int i = 0; i < split.length; i++) {
+            String row = split[i];
+            for (int j = 0; j < row.length() ; j++) {
+                char c = row.charAt(j);
+                if (c != '.') {
+                    blocks[i][j] = new Block(c);
+                }
+            }
+        }
     }
 
     @Override
     public String toString() {
-        return s;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks[i].length; j++ ) {
+                Block b = blocks[i][j];
+                if (b == null) {
+                    sb.append(".");
+                } else {
+                    sb.append(b.getColor());
+                }
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public Piece rotateRight() {
+        return null;
     }
 }
--- src/test/java/tetris/RotatingPiecesOfBlocksTest.java
+++ src/test/java/tetris/RotatingPiecesOfBlocksTest.java
@@ -40,14 +40,22 @@ public class RotatingPiecesOfBlocksTest extends Assert {
                     "...\n", piece.toString());
         }
 
-//        @Test
-//        public void can_be_rotated_right() {
-//            piece = piece.rotateRight();
-//            assertEquals("" +
-//                    "...\n" +
-//                    ".XX\n" +
-//                    "...\n", piece.toString());
-//        }
+        // 0,0 -> 0,c-1
+        // 0,2 -> 1,c-1
+        // 0,3 -> r-1,c-1
+
+        // 1,0 -> 0,1
+        // 1,1 -> 1,1
+        // 1,c-1 -> r-1,1
+        @Ignore
+        @Test
+        public void can_be_rotated_right() {
+            piece = piece.rotateRight();
+            assertEquals("" +
+                    "...\n" +
+                    ".XX\n" +
+                    "...\n", piece.toString());
+        }
 
 //        @Test
 //        public void can_be_rotated_left() {
