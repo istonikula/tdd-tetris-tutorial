fa2f4f7765a39435225bcf1e269c3074ccce1800
Isto Nikula <isto.nikula@iki.fi>
2015-04-17 14:09:12 +0300

    FallingPiecesTest When_a_piece_reaches_the_bottom it_is_still_falling_on_the_last_row

--- src/test/java/tetris/FallingPiecesTest.java
+++ src/test/java/tetris/FallingPiecesTest.java
@@ -41,7 +41,6 @@ public class FallingPiecesTest extends Assert {
         }
     }
 
-/*
     public class When_a_piece_reaches_the_bottom {
 
         @Before
@@ -78,7 +77,6 @@ public class FallingPiecesTest extends Assert {
 //            assertFalse(board.hasFalling());
 //        }
     }
-*/
 
 /*
     public class When_a_piece_lands_on_another_piece {
